62510fff2b635a6a7d10e05f0939744a03b0ae95
Cunha-Renato <cunhamrenato@gmail.com>
2026-04-07 12:53:53 -0300

    audio

--- modules/misc.nix
+++ modules/misc.nix
@@ -15,5 +15,14 @@
         alsa.support32Bit = true;
         pulse.enable = true;
         jack.enable = true;
+
+        extraConfig.pipewire."92-low-latency" = {
+            context.properties = {
+                default.clock.rate = 48000;
+                default.clock.quantum = 1024;
+                default.clock.min-quantum = 1024;
+                default.clock.max-quantum = 1024;
+            };
+        };
     };
 }
